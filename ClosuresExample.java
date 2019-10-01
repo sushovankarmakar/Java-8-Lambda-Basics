284d881a1ba83081fa13d2c75fd63febf11983dd
Sushovan <skarmakar@internal.tavisca.com>
2019-10-01 10:39:47 +0530

    Closures in Lambda Expressions

---
+++ ClosuresExample.java
@@ -0,0 +1,18 @@
+package io.java8;
+
+public class ClosuresExample {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		doProcess(a, i -> System.out.println(i + b));
+	}
+	
+	public static void doProcess(int i, Process p) {
+		p.process(i);
+	}
+}
+
+interface Process {
+	void process(int i);
+}
